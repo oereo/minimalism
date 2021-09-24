a969dfe5d7e551687d88ba7c19345a6e602b2450
oereo <dlstpgns0406@gmail.com>
2021-09-25 00:31:15 +0900

    feat: apply urls setting

---
+++ minimalism/accounts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+import accounts.views
+
+urlpatterns = [
+    # path()
+]
---
+++ minimalism/diagnostic_test/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+import diagnostic_test.views
+
+urlpatterns = [
+    # path()
+]
