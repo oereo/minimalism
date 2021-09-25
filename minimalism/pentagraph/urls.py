888db96afe073d1f344af999b293cd03f940cf06
oereo <dlstpgns0406@gmail.com>
2021-09-25 14:36:13 +0900

    feat: add plan-delete function

--- minimalism/pentagraph/urls.py
+++ minimalism/pentagraph/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("main/", views.pentagraphmain, name="pentagraphmain"),
 
-    path("create-plan", views.create_plan, name="create_plan")
+    path("create-plan", views.create_plan, name="create_plan"),
+    path("delete-plan/<int:pk>", views.delete_plan, name="delete_plan")
+
 ]
--- minimalism/pentagraph/views.py
+++ minimalism/pentagraph/views.py
@@ -8,8 +8,30 @@ from tagcloud.models import Plan
 def pentagraphmain(request):
     user = request.user
     plans = Plan.objects.filter(user=user)
+    plan1 = Plan.objects.filter(user=user, status=1)
+    plan2 = Plan.objects.filter(user=user, status=2)
 
-    return render(request, "pentagraph.html", {'plans': plans})
+    plan3 = Plan.objects.filter(user=user, status=3)
+
+    plan4 = Plan.objects.filter(user=user, status=4)
+
+    plan5 = Plan.objects.filter(user=user, status=5)
+
+    plan1_length = len(plan1)
+    plan2_length = len(plan2)
+
+    plan3_length = len(plan3)
+    plan4_length = len(plan4)
+    plan5_length = len(plan5)
+
+    return render(request, "pentagraph.html",
+                  {'plans': plans,
+                   'plan1_length': plan1_length,
+                   'plan2_length': plan2_length,
+                   'plan3_length': plan3_length,
+                   'plan4_length': plan4_length,
+                   'plan5_length': plan5_length,
+                   })
 
 
 def create_plan(request):
@@ -27,4 +49,10 @@ def create_plan(request):
             user=user
         )
         return redirect('pentagraphmain')
+    return redirect('pentagraphmain')
+
+
+def delete_plan(request, pk):
+    plan = get_object_or_404(Plan, pk=pk)
+    plan.delete()
     return redirect('pentagraphmain')
