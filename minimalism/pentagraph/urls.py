0e45ac51bf068279e8690b675acbfed9b9da9cd1
oereo <dlstpgns0406@gmail.com>
2021-09-25 13:52:03 +0900

    feat: add create-plan function

--- minimalism/pentagraph/urls.py
+++ minimalism/pentagraph/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("main/",views.pentagraphmain,name="pentagraphmain"),
+    path("main/", views.pentagraphmain, name="pentagraphmain"),
+
+    path("create-plan", views.create_plan, name="create_plan")
 ]
--- minimalism/pentagraph/views.py
+++ minimalism/pentagraph/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render, redirect
 
 from os import error
-from django.shortcuts import redirect, render,get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
+from tagcloud.models import Plan
 
-# Create your views here.
+
 def pentagraphmain(request):
-    return render(request, "pentagraph.html")
+    user = request.user
+    plans = Plan.objects.filter(user=user)
+
+    return render(request, "pentagraph.html", {'plans': plans})
+
+
+def create_plan(request):
+    user = request.user
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        date = request.POST['date']
+        status = request.POST['drop1']
+        Plan.objects.create(
+            title=title,
+            content=content,
+            clean_date=date,
+            status=status,
+            user=user
+        )
+        return redirect('pentagraphmain')
+    return redirect('pentagraphmain')
