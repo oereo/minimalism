d7edeca1cb8f0779109e815da54aa92392d42dd9
oereo <dlstpgns0406@gmail.com>
2021-09-25 10:57:48 +0900

    feat: add auth functions

--- minimalism/accounts/urls.py
+++ minimalism/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-import accounts.views
+from accounts.views import login, create_user, signup, signup_user
 
 urlpatterns = [
-    # path()
+    path('login/', login, name="login"),
+    path('login-user', create_user, name='create_user'),
+    path('signup/', signup, name="signup"),
+    path('signup-user', signup_user, name="signup_user")
 ]
--- minimalism/accounts/views.py
+++ minimalism/accounts/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+from django.contrib import auth
 
-# Create your views here.
+# from .models import SocialTokens
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def signup_user(request):
+    if request.method == "POST":
+        if request.POST["password1"] == request.POST["password2"]:
+            user = User.objects.create_user(
+                username=request.POST["username"],
+                password=request.POST["password1"],
+                email=request.POST["email"]
+            )
+            auth.login(request, user)
+            return redirect('landingPage')
+        return render(request, 'signup.html')
+    return render(request, 'signup.html')
+
+
+def create_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            print(user)
+            return redirect('landingPage')
+        else:
+            return render(request, 'login.html')
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('landingPage')
